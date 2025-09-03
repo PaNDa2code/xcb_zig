5efaee3e1ce55c21aaba3ffe5a0bc06fe5117535
PaNDa2code <moaaz0688@gmail.com>
2025-09-03 01:37:44 -0400

    chore: minimize python calls

--- build.zig
+++ build.zig
@@ -7,12 +7,12 @@ pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
     // const no_docs = b.option(bool, "no-docs", "skip installing documentation") orelse false;
-    // const linkage: std.builtin.LinkMode =
-    //     b.option(std.builtin.LinkMode, "linkage", "whether to statically or dynamically link the library") orelse
-    //     if (target.result.isGnuLibC())
-    //         .dynamic
-    //     else
-    //         .static;
+    const linkage: std.builtin.LinkMode =
+        b.option(std.builtin.LinkMode, "linkage", "whether to statically or dynamically link the library") orelse
+        if (target.result.isGnuLibC())
+            .dynamic
+        else
+            .static;
 
     // const clap = b.dependency("clap", .{
     //     .target = target,
@@ -22,21 +22,18 @@ pub fn build(b: *std.Build) !void {
     const libxcb_upstream = b.dependency("libxcb", .{});
     const xcbproto_upstream = b.dependency("xcbproto", .{});
 
-    // const libxau = b.dependency("libxau", .{
-    //     .target = target,
-    //     .optimize = optimize,
-    //     .linkage = linkage,
-    // });
+    const libxau = b.dependency("libxau", .{
+        .target = target,
+        .optimize = optimize,
+        .linkage = linkage,
+    });
 
-    // const xorgproto_upstream = libxau.builder.dependency("xorgproto", .{});
+    const xorgproto_upstream = libxau.builder.dependency("xorgproto", .{});
 
-    // const xcb_util_upstream = b.dependency("xcb-util", .{});
-    // const xcb_util_image_upstream = b.dependency("xcb-util-image", .{});
+    const xcb_util_upstream = b.dependency("xcb-util", .{});
+    const xcb_util_image_upstream = b.dependency("xcb-util-image", .{});
 
     const python3_path = try b.findProgram(&.{ "python3", "python" }, &.{});
-
-    var xproto_c_sources_base: Build.LazyPath = undefined;
-    var xproto_c_sources = try std.ArrayList([]const u8).initCapacity(b.allocator, 0);
 
     const libxcb_mod = b.createModule(.{
         .target = target,
@@ -49,67 +46,118 @@ pub fn build(b: *std.Build) !void {
 
     libxcb.linkLibC();
 
+    const headers = b.addWriteFiles();
+
     {
-        const libxcb_src = libxcb_upstream.path("src");
-        const libxcb_c_client = libxcb_upstream.path("src/c_client.py");
+        const c_client_py = libxcb_upstream.path("src/c_client.py");
 
-        xproto_c_sources_base = libxcb_src;
+        for (xml_files) |xml_file_basename| {
+            const c_file_basename = b.fmt("{s}.c", .{xml_file_basename[0 .. xml_file_basename.len - 4]});
+            const h_file_basename = b.fmt("{s}.h", .{xml_file_basename[0 .. xml_file_basename.len - 4]});
 
-        for (xml_files) |xml_file| {
-            const c_file = b.fmt("{s}.c", .{xml_file[0 .. xml_file.len - 4]});
+            const src_xml_file = b.fmt("src/{s}", .{xml_file_basename});
 
-            const cmd = b.addSystemCommand(&.{python3_path});
-            cmd.addFileArg(libxcb_c_client);
-            cmd.addArgs(&.{ "-c", "libxcb", "-l", "libxcb", "-s", "3", "-p" });
-            cmd.addDirectoryArg(xcbproto_upstream.path(""));
-            cmd.addFileArg(xcbproto_upstream.path(b.fmt("src/{s}", .{xml_file})));
+            const py = b.addSystemCommand(&.{python3_path});
+            py.addFileArg(c_client_py);
 
-            cmd.setCwd(libxcb_src);
+            py.addArgs(&.{ "-c", "libxcb", "-l", "libxcb", "-s", "3", "-p" });
+            py.addDirectoryArg(xcbproto_upstream.path(""));
+            py.addFileArg(xcbproto_upstream.path(src_xml_file));
 
-            const xproto_c_file_path = cwdOutFile(b, cmd, c_file);
+            py.setCwd(libxcb_upstream.path("src"));
 
-            // libxcb.step.dependOn(&cmd.step);
+            // not the greatest but works
+            const cat_c = b.addSystemCommand(&.{"cat"});
+            cat_c.setCwd(py.cwd.?);
+            cat_c.addArg(c_file_basename);
+
+            const c_file = cat_c.captureStdOut();
+            cat_c.captured_stdout.?.basename = c_file_basename;
+
+            const cat_h = b.addSystemCommand(&.{"cat"});
+            cat_h.setCwd(py.cwd.?);
+            cat_h.addArg(h_file_basename);
+
+            const h_file = cat_h.captureStdOut();
+            cat_h.captured_stdout.?.basename = h_file_basename;
 
             libxcb.addCSourceFile(.{
-                .file = xproto_c_file_path,
+                .file = c_file,
                 .flags = &.{
                     "-DXCB_QUEUE_BUFFER_SIZE=16384",
                     "-DIOV_MAX=16",
                 },
             });
 
-            try xproto_c_sources.append(b.allocator, c_file);
+            _ = headers.addCopyFile(
+                h_file,
+                b.fmt("xcb/{s}", .{h_file_basename}),
+            );
         }
     }
 
+    _ = headers.addCopyDirectory(
+        xorgproto_upstream.path("include"),
+        "",
+        .{ .include_extensions = &.{".h"} },
+    );
+
+    _ = headers.addCopyFile(libxcb_upstream.path("src/xcb.h"), "xcb/xcb.h");
+    _ = headers.addCopyFile(xcb_util_upstream.path("src/xcb_atom.h"), "xcb/xcb_atom.h");
+    _ = headers.addCopyFile(xcb_util_upstream.path("src/xcb_aux.h"), "xcb/xcb_aux.h");
+    _ = headers.addCopyFile(xcb_util_upstream.path("src/xcb_event.h"), "xcb/xcb_event.h");
+    _ = headers.addCopyFile(xcb_util_upstream.path("src/xcb_util.h"), "xcb/xcb_util.h");
+    _ = headers.addCopyFile(xcb_util_image_upstream.path("image/xcb_bitops.h"), "xcb/xcb_bitops.h");
+    _ = headers.addCopyFile(xcb_util_image_upstream.path("image/xcb_image.h"), "xcb/xcb_image.h");
+    _ = headers.addCopyFile(xcb_util_image_upstream.path("image/xcb_pixel.h"), "xcb/xcb_pixel.h");
+
     libxcb.addCSourceFiles(.{
-        .files = xproto_c_sources.items,
-        .root = xproto_c_sources_base,
+        .root = libxcb_upstream.path("."),
+        .files = &.{
+            "src/xcb_auth.c",
+            "src/xcb_conn.c",
+            "src/xcb_ext.c",
+            "src/xcb_in.c",
+            "src/xcb_list.c",
+            "src/xcb_out.c",
+            "src/xcb_util.c",
+            "src/xcb_xid.c",
+        },
         .flags = &.{
             "-DXCB_QUEUE_BUFFER_SIZE=16384",
             "-DIOV_MAX=16",
         },
     });
 
-    libxcb.installHeadersDirectory(
-        xproto_c_sources_base,
-        "",
-        .{},
-    );
+    libxcb.addIncludePath(libxcb_upstream.path("src"));
+    libxcb.addIncludePath(libxcb_upstream.path("include"));
+    libxcb.addIncludePath(xorgproto_upstream.path("include"));
+    libxcb.addIncludePath(headers.getDirectory().path(b, "include"));
+
+    libxcb.linkLibrary(libxau.artifact("Xau"));
+
+    libxcb.installHeadersDirectory(headers.getDirectory(), "", .{});
+    libxcb.addIncludePath(headers.getDirectory());
 
     b.installArtifact(libxcb);
 }
 
-fn cwdOutFile(b: *Build, run: *Build.Step.Run, path: []const u8) Build.LazyPath {
+fn cwdOutFile(b: *Build, run: *Build.Step.Run, basename: []const u8) Build.LazyPath {
     const cwd = run.cwd.?;
-    const abs_path = cwd.join(b.allocator, path) catch @panic("OOM");
-    const gen_file = b.allocator.create( Build.GeneratedFile ) catch @panic("OOM");
-    gen_file.* = .{ .step = &run.step, .path = abs_path.getPath(b) };
-    return .{
-        .generated = .{
-            .file = gen_file,
-        },
+    const abs_path = cwd.join(b.allocator, basename) catch @panic("OOM");
+    const output = b.allocator.create(Build.Step.Run.Output) catch @panic("OOM");
+
+    output.* = .{
+        .prefix = "",
+        .basename = basename,
+        .generated_file = .{ .step = &run.step, .path = abs_path.getPath(b) },
     };
+
+    if (run.rename_step_with_output_arg) {
+        run.setName(b.fmt("{s} ({s})", .{ run.step.name, basename }));
+    }
+
+    return .{ .generated = .{ .file = &output.generated_file } };
 }
 
 const xml_files = [_][]const u8{
